93bfaf1361cb9f7c10c8df1f36da546bf0ca2224
Nirmal Bam <codernirmalbam@gmail.com>
2024-10-15 17:41:59 +0545

    Read data token from blockchain . Add token input value and usdt input value

--- src/pages/SwapToken.jsx
+++ src/pages/SwapToken.jsx
@@ -1,10 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../components/Button";
 import { FaCaretDown, FaExchangeAlt } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { ethers } from "ethers";
 
 export default function SwapToken() {
   const [selectedToken, setSelectedToken] = useState("Craft Token");
   const [isShowTokenList, setIsShowTokenList] = useState(false);
+  const [userTokenInfo, setUserTokenInfo] = useState("0");
+  const web3ContentInitialState = useSelector((state) => state.web3Content);
+  const [tokenInputValue, setTokenInputValue] = useState(1);
+  const [usdtInputValue, setUsdtInputValue] = useState(1);
+
+  useEffect(() => {
+    const getUserToken = async () => {
+      try {
+        let data;
+        if (selectedToken === "Craft Token") {
+          data =
+            await web3ContentInitialState.accountInfo.craftTokenContract.balanceOf(
+              web3ContentInitialState.accountInfo.selectedAccount
+            );
+        } else {
+          if (selectedToken === "Dragon Craft Token") {
+            data =
+              await web3ContentInitialState.accountInfo.dragonCraftTokenContract.balanceOf(
+                web3ContentInitialState.accountInfo.selectedAccount
+              );
+          } else {
+            data = null;
+          }
+        }
+        const tokenInEthers = ethers.formatEther(data);
+        setUserTokenInfo(tokenInEthers);
+      } catch (error) {
+        console.log("An error occurred : ", error);
+      }
+    };
+
+    web3ContentInitialState.accountInfo.craftTokenContract && getUserToken();
+  }, [
+    web3ContentInitialState.accountInfo?.craftTokenContract || null,
+    web3ContentInitialState.accountInfo?.selectedAccount || null,
+    selectedToken,
+  ]);
+
+  function onChangeStakeTokenInputValue(e) {
+    setTokenInputValue(e.target.value);
+    if (e.target.value != "") {
+      setUsdtInputValue(parseInt(e.target.value));
+    } else {
+      setUsdtInputValue(0);
+    }
+  }
 
   return (
     <>
@@ -12,24 +60,50 @@ export default function SwapToken() {
         <div className="max-w-[1200px] mx-auto w-full  flex justify-center px-3">
           <div className="max-w-xl w-full bg-s2 p-10 flex flex-col rounded-7xl">
             <h1 className="text-2xl md:text-3xl font-bold mt-2">Swap Token</h1>
-
             <div className="mt-7">
               <div className="flex justify-between items-center">
                 <div className="flex gap-2 items-center">
                   <span className="text-gray-400">From</span>
-                  <div className="flex items-center cursor-pointer relative" onClick={() => setIsShowTokenList(!isShowTokenList)}>
+                  <div
+                    className="flex items-center cursor-pointer relative"
+                    onClick={() => setIsShowTokenList(!isShowTokenList)}
+                  >
                     <span className="text-lg">{selectedToken}</span>
-                    <FaCaretDown size={20}/>
-                    <div className={`absolute max-w-sm w-[10rem] rounded-xl top-8 bg-s3 z-20 p-2 transition-all ${isShowTokenList ? "scale-100" : "scale-0"}`}>
-                       <ul>
-                        <li className="hover:bg-s4 p-2 rounded-md" onClick={()=> setSelectedToken("Craft Token")}>Craft Token</li>
-                        <li className="hover:bg-s4 p-2 rounded-md" onClick={()=> setSelectedToken("Dragon Craft Token")}>Dragon Craft Token</li>
-                       </ul>
+                    <FaCaretDown size={20} />
+                    <div
+                      className={`absolute max-w-sm w-[10rem] rounded-xl top-8 bg-s3 z-20 p-2 transition-all ${
+                        isShowTokenList ? "scale-100" : "scale-0"
+                      }`}
+                    >
+                      <ul className="flex flex-col gap-2">
+                        <li
+                          className={`${
+                            selectedToken === "Craft Token"
+                              ? "bg-s4"
+                              : "hover:bg-s4/[0.5]"
+                          }  p-2 rounded-md`}
+                          onClick={() => setSelectedToken("Craft Token")}
+                        >
+                          Craft Token
+                        </li>
+                        <li
+                          className={`${
+                            selectedToken === "Dragon Craft Token"
+                              ? "bg-s4"
+                              : "hover:bg-s4/[0.5]"
+                          }  p-2 rounded-md`}
+                          onClick={() => setSelectedToken("Dragon Craft Token")}
+                        >
+                          Dragon Craft Token
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </div>
                 <div className="flex items-center">
-                  <span className="text-gray-400">Current Token :- 4000</span>
+                  <span className="text-gray-400 max-w-[14rem]">
+                    Token :- {Number(userTokenInfo)}
+                  </span>
                 </div>
               </div>
 
@@ -37,6 +111,8 @@ export default function SwapToken() {
                 <input
                   type="number"
                   className="w-full bg-s1 p-3 rounded-lg border-none focus:outline-primary"
+                  value={tokenInputValue}
+                  onChange={onChangeStakeTokenInputValue}
                   min={1}
                 />
               </div>
@@ -45,7 +121,6 @@ export default function SwapToken() {
               <FaExchangeAlt
                 className="-rotate-90 transition-all hover:scale-110 cursor-pointer bg-primary/[0.1] hover:bg-primary/[0.2] p-3 rounded-full"
                 size={45}
-                
               />
             </div>
 
@@ -57,16 +132,14 @@ export default function SwapToken() {
                     <span className="text-lg">USDT</span>
                   </div>
                 </div>
-                <div className="flex items-center">
-                  <span className="text-gray-400">Current Token :- 50</span>
-                </div>
               </div>
 
               <div className="mt-3">
                 <input
                   type="number"
-                  className="w-full bg-s1 p-3 rounded-lg border-none focus:outline-primary"
-                  min={1}
+                  className="w-full disabled:bg-s1 p-3 rounded-lg border-none focus:outline-primary"
+                  disabled
+                  value={usdtInputValue}
                 />
               </div>
             </div>
